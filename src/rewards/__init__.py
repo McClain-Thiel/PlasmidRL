9c06457329cc3ab3d46a8e43faf132fd45df63e6
mcclain <mcclain.thiel@gmail.com>
2025-10-26 09:26:53 +0000

    hm

--- src/rewards/__init__.py
+++ src/rewards/__init__.py
@@ -1,12 +1,8 @@
-from .rewards import score_completions
-# The reward functions are expected to accept:
+from .scorer import Scorer
+from .reward_config import RewardConfig
 
-# completions: list of model outputs for the batch
-# other keyword arguments corresponding to columns in your dataset (e.g. solution, ground_truth, image, etc.)
-# Optionally, **kwargs to absorb extra parameters (like trainer_state)
-# this always has the same signature so I just define it here once.
-def Score(
-    completions: list,
-    **kwargs,
-) -> list[float]:
-    return score_completions(completions)
+config = RewardConfig(
+    
+)
+
+score = Scorer(RewardConfig()).score
--- src/rewards/scorer.py
+++ src/rewards/scorer.py
@@ -1,6 +1,8 @@
 from rewards.reward_config import RewardConfig
 import plasmidkit as pk
 import pandas as pd
+from typing import Any
+from concurrent.futures import ThreadPoolExecutor
 
 
 class Scorer:
@@ -8,11 +10,17 @@ class Scorer:
         self.reward_config = reward_config
         self.score_functions = [self.score_ori, self.score_promoter, self.score_terminator, self.score_marker, self.score_cds]
         self.weights = [self.reward_config.ori_weight, self.reward_config.promoter_weight, self.reward_config.terminator_weight, self.reward_config.marker_weight, self.reward_config.cds_weight]
+        total_weight = sum(self.weights)
+        if total_weight:
+            self.weights = [w / total_weight for w in self.weights]
+        else:
+            uniform_weight = 1.0 / len(self.score_functions)
+            self.weights = [uniform_weight for _ in self.score_functions]
 
     def annotate(self, sequence: str) -> Any:
         return pk.annotate(sequence, is_sequence=True)
 
-    def runner(self, sequence: str, annotations: Any) -> float:
+    def runner(self, sequence: str, annotations: Any) -> list[float]:
         """
         Takes a list of runnable functions with the same signature and runs them in parallel.
 
@@ -21,24 +29,30 @@ class Scorer:
             annotations: The annotations to score
 
         Returns:
-            The score
+            A list of scores in the same order as `self.score_functions`.
         """
         with ThreadPoolExecutor(max_workers=len(self.score_functions)) as executor:
             futures = [executor.submit(func, sequence, annotations) for func in self.score_functions]
             return [future.result() for future in futures]
-        pass
 
-    def score_ori(self, annotations: pd.DataFrame) -> float:
+    def score_ori(self, seq: str, annotations: pd.DataFrame) -> float:
         return 0.0
 
-    def score_promoter(self, annotations: pd.DataFrame) -> float:
+    def score_promoter(self, seq: str, annotations: pd.DataFrame) -> float:
         return 0.0
 
-    def score_terminator(self, annotations: pd.DataFrame) -> float:
+    def score_terminator(self, seq: str, annotations: pd.DataFrame) -> float:
         return 0.0
 
-    def score_marker(self, annotations: pd.DataFrame) -> float:
+    def score_marker(self, seq: str, annotations: pd.DataFrame) -> float:
         return 0.0
 
-    def score_cds(self, annotations: pd.DataFrame) -> float:
+    def score_cds(self, seq: str, annotations: pd.DataFrame) -> float:
         return 0.0
+
+    def score_length(self, seq: str, annotations: pd.DataFrame) -> float:
+        return 0.0
+
+    def score(self, seq: str, annotations: pd.DataFrame) -> float:
+        results = self.runner(seq, annotations)
+        return sum(w * r for w, r in zip(self.weights, results))
